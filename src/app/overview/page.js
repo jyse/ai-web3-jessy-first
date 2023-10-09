c49b4f55fa0ce85c7770b720d89853c0ffcc637c
jyse <jessythe@gmail.com>
2023-10-09 23:01:14 +0200

    responsive layout overview content and ai art generator content

--- src/app/overview/page.js
+++ src/app/overview/page.js
@@ -28,113 +28,98 @@ const stylesPrompt = [
 const OverviewPage = () => {
   return (
     <MainContainer>
-      <div className={styles.welcomeContent}>
-        <div className={styles.welcomeText}>
-          <h1>Welcome at ArtIVerse Studio!🎨🔥</h1>
-          <h2>Art made for the Metaverse</h2>
-        </div>
-        <div className={styles.stepsContainer}>
-          <div className={styles.step}>
-            <div className={styles.stepIntro}>
-              <div className={styles.icon}>🎨</div>
-              <div className={styles.title}>
-                <h2>Create art with AI</h2>
-                <h3>Describe what you want and generate new images.</h3>
-              </div>
-            </div>
-            <div className={styles.stepContent}>
-              <div className={styles.gridAiStyles}>
-                {stylesPrompt?.map((styleForPrompt, index) => {
-                  return (
-                    <div className={styles.styleBox} key={index}>
-                      <Link href={`/ai-art`}>
-                        <div className={styles.styleBoxTitle}>
-                          <h2>{styleForPrompt.name}</h2>
-                        </div>
-                        <div className={styles.rowAIStyles}>
-                          {[1, 2, 3, 4].map((imageIndex) => {
-                            return (
-                              <Image
-                                key={imageIndex}
-                                src={`${styleForPrompt.path}${imageIndex}.png`}
-                                width={85}
-                                height={85}
-                                alt={styleForPrompt.name}
-                                style={
-                                  (imageIndex == 1 && firstImageAIStyle) ||
-                                  (imageIndex == 4 && lastImageAIStyle)
-                                }
-                                priority={true}
-                              />
-                            );
-                          })}
-                        </div>
-                      </Link>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
+      <div className={styles.overviewContainer}>
+        <div className={styles.overviewContent}>
+          <div className={styles.welcomeText}>
+            <h1>Welcome at ArtIVerse Studio!🎨🔥</h1>
+            <h2>Art made for the Metaverse</h2>
           </div>
-          <div className={styles.step}>
-            <div className={styles.stepIntro}>
-              <div className={styles.icon}>🖼️</div>
-              <div className={styles.title}>
-                <h2> Turn art into a NFT collection</h2>
-                <h3> Add more images to your current collection</h3>
+          <div className={styles.stepsContainer}>
+            <div className={styles.step}>
+              <div className={styles.stepIntro}>
+                <div className={styles.icon}>🎨</div>
+                <div className={styles.title}>
+                  <h2>Create art with AI</h2>
+                  <h3>Describe what you want and generate new images.</h3>
+                </div>
+              </div>
+              <div className={styles.stepContent}>
+                <div className={styles.gridAiStyles}>
+                  {stylesPrompt?.map((styleForPrompt, index) => {
+                    return (
+                      <div className={styles.styleBox} key={index}>
+                        <Link href={`/ai-art`}>
+                          <div className={styles.styleBoxTitle}>
+                            <h2>{styleForPrompt.name}</h2>
+                          </div>
+                          <div className={styles.rowAIStyles}>
+                            {[1, 2, 3, 4].map((imageIndex) => {
+                              return (
+                                <Image
+                                  key={imageIndex}
+                                  src={`${styleForPrompt.path}${imageIndex}.png`}
+                                  width={80}
+                                  height={80}
+                                  alt={styleForPrompt.name}
+                                  style={
+                                    (imageIndex == 1 && firstImageAIStyle) ||
+                                    (imageIndex == 4 && lastImageAIStyle)
+                                  }
+                                  priority={true}
+                                />
+                              );
+                            })}
+                          </div>
+                        </Link>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
-            <div className={styles.stepContent}>
-              {/* //comment: work on the spacing here at the last image */}
-              <RowGallery>
-                <Image
-                  src="/gen-imgs-to-add/1.png"
-                  width={250}
-                  height={250}
-                  alt="Character"
-                  style={styleGenImgsToAdd}
-                  priority={true}
-                />
-                <Image
-                  src="/gen-imgs-to-add/2.png"
-                  width={250}
-                  height={250}
-                  alt="Character"
-                  style={styleGenImgsToAdd}
-                  priority={true}
-                />
-                <Image
-                  src="/gen-imgs-to-add/3.png"
-                  width={250}
-                  height={250}
-                  alt="Character"
-                  style={styleGenImgsToAdd}
-                  priority={true}
-                />
-                <Image
-                  src="/gen-imgs-to-add/4.png"
-                  width={250}
-                  height={250}
-                  alt="Character"
-                  style={styleGenImgsToAdd}
-                  priority={true}
-                />
-              </RowGallery>
-            </div>
-          </div>
-          <div className={styles.step}>
-            <div className={styles.stepIntro}>
-              <div className={styles.icon}>💰</div>
-              <div className={styles.title}>
-                <h2> Publish NFT collection on a marketplace for sale!</h2>
-                <h3> Let's mint!</h3>
+            <div className={styles.step}>
+              <div className={styles.stepIntro}>
+                <div className={styles.icon}>🖼️</div>
+                <div className={styles.title}>
+                  <h2> Turn art into a NFT collection</h2>
+                  <h3> Add more images to your current collection</h3>
+                </div>
+              </div>
+              <div className={styles.stepContent}>
+                <RowGallery>
+                  {[1, 2, 3, 4].map((imageIndex) => {
+                    return (
+                      <Image
+                        key={imageIndex}
+                        src={`/output/genImgs/${imageIndex}.png`}
+                        width={250}
+                        height={250}
+                        alt={"Image to be Added"}
+                        style={
+                          (imageIndex == 1 && firstImageAIStyle) ||
+                          (imageIndex == 4 && lastImageAIStyle)
+                        }
+                        priority={true}
+                      />
+                    );
+                  })}
+                </RowGallery>
               </div>
             </div>
-            <div className={styles.stepContent}>
-              <h2>The goal is to have our collection here </h2>
-              {/* <Link href="https://testnets.opensea.io/...">
+            <div className={styles.step}>
+              <div className={styles.stepIntro}>
+                <div className={styles.icon}>💰</div>
+                <div className={styles.title}>
+                  <h2> Publish NFT collection on a marketplace for sale!</h2>
+                  <h3> Let's mint!</h3>
+                </div>
+              </div>
+              <div className={styles.stepContent}>
+                <h2>The goal is to have our collection here </h2>
+                {/* <Link href="https://testnets.opensea.io/...">
                <h2>here</h2>
                </Link>  */}
+              </div>
             </div>
           </div>
         </div>
